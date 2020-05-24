4dccf343ef923581a33edecd3f7876f6649cebee
Fernando Villasenor <fvilla18@gmail.com>
2020-05-24 00:08:06 -0700

    Add layout

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,51 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { Container, Box, AppBar, Toolbar, Typography, Grid } from '@material-ui/core';
+import FeedContainer from '../components/FeedContainer';
+import SideBar from '../components/SideBar';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.light,
+    height: '100vh'
+  },
+  appBar: {
+    backgroundColor: theme.palette.background.default
+  },
+  toolbar: {
+    color: theme.palette.text.dark
+  }
+}));
 
 const Home = () => {
-  return <div>Home</div>;
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar className={classes.appBar} position="static" elevation={0}>
+        <Toolbar className={classes.toolbar} variant="dense">
+          <Typography variant="h6" color="inherit">
+            Infinite Scroll
+          </Typography>
+        </Toolbar>
+      </AppBar>
+
+      <Box px={3} py={3}>
+        <Container maxWidth="md">
+          <Grid container spacing={3}>
+            <Grid item xs={9}>
+              <FeedContainer />
+            </Grid>
+
+            <Grid item xs={3}>
+              <SideBar />
+            </Grid>
+          </Grid>
+        </Container>
+      </Box>
+    </div>
+  );
 };
 
 export default Home;
--- src/theme.js
+++ src/theme.js
@@ -1,10 +1,10 @@
 import { createMuiTheme } from '@material-ui/core/styles';
-import { red } from '@material-ui/core/colors';
+import { red, grey, blueGrey } from '@material-ui/core/colors';
 
 const theme = createMuiTheme({
   palette: {
     primary: {
-      main: '#556cd6'
+      main: '#2c4871'
     },
     secondary: {
       main: '#19857b'
@@ -13,7 +13,11 @@ const theme = createMuiTheme({
       main: red.A400
     },
     background: {
-      default: '#fff'
+      default: '#fff',
+      light: blueGrey[100]
+    },
+    text: {
+      dark: grey[900]
     }
   }
 });
